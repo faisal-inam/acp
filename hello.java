fab3695354d75ebe05d2b01361360e6cc5b74fff
faisal-inam <87889202+faisal-inam@users.noreply.github.com>
2022-04-07 02:18:14 +0500

    Update hello.java
    Better exclamation

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
     public static void main(String args[])
     {
-        System.out.println("Welcome to Advanced Computer Programming!");
+        System.out.println("Welcome to Advanced Computer Programming!!");
     }
 }
